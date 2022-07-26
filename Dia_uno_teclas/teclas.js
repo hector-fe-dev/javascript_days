7219e21a6eb53df651ecb92391d3552a7fe69ad3
Hector del Reguero Valle <hector.drv.frontend@gmail.com>
2022-07-25 21:43:04 -0500

    Terminamos el ejercicio de las teclas

---
+++ Dia_uno_teclas/teclas.js
@@ -0,0 +1,24 @@
+document.addEventListener('keydown', (event) => {
+    play(event)
+});
+const keys = document.querySelectorAll(".card");
+keys.forEach(key=>key.addEventListener('transitionend',prueba))
+    
+    
+
+var play = (e) => {
+    let audio = document.getElementById(`a${e.keyCode}`)
+    let key =  document.getElementById(`k${e.keyCode}`)
+
+    audio.currentTime = 0;
+    audio.play()
+    key.classList.add("shine")
+}
+
+
+function prueba (e){
+    if (e.propertyName === 'transform'){
+        this.classList.remove('shine');
+    }
+}
+
